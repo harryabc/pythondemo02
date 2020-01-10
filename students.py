badf2924b067544dbd79ca7339ebbd0e837627b6
harryabc <harryabc@sina.com>
2020-01-10 09:46:39 +0800

    Create students.py

---
+++ students.py
@@ -0,0 +1,21 @@
+n = int(input("Enter the number of students: "))
+data = {} # 用来存储数据的字典变量
+Subjects = ('Physics', 'Maths', 'History') # 所有科目
+try:
+    for i in range(0, n):
+        name = input('Enter the name of the student {}: '.format(i + 1)) # 获得学生名称
+        marks = []
+        for x in Subjects:
+            marks.append(int(input('Enter marks of {}: '.format(x)))) # 获得每一科的分数
+        data[name] = marks
+    for x, y in data.items():
+        total =  sum(y)
+        print("{}'s total marks is {}".format(x, total))
+        if total < 180:
+            print(x, "failed :(")
+        else:
+            print(x, "passed :)")
+except ValueError:
+    print('输入错误，分数只能为数字')
+else:
+    print('没有异常')
